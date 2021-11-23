6c62017477bbb4fb9f04a2d35635d7edeedb7c7e
Liu <wei.liu.457@my.csun.edu>
2021-11-22 18:03:00 -0800

    dropdown menu bar

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var menulist = document.getElementById("menulist");
 menulist.style.maxHeight = "0px";
 function menutoggle() {
     if (menulist.style.maxHeight == "0px") {
-        menulist.style.maxHeight = "130px";
+        menulist.style.maxHeight = "200px";
     }
     else {
         menulist.style.maxHeight = "0px";
     }
 }
+s
