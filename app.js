387f1f10f766aa2a42a3006e7970871044b9dd2c
Liu <wei.liu.457@my.csun.edu>
2021-12-01 19:43:31 -0800

    footer and menu nav update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var menulist = document.getElementById("menulist");
-menulist.style.maxHeight = "0px";
-function menutoggle() {
-    if (menulist.style.maxHeight == "0px") {
-        menulist.style.maxHeight = "200px";
-    }
-    else {
-        menulist.style.maxHeight = "0px";
-    }
-}
-s
+// var menulist = document.getElementById("menulist");
+// menulist.style.maxHeight = "0px";
+// function menutoggle() {
+//     if (menulist.style.maxHeight == "0px") {
+//         menulist.style.maxHeight = "200px";
+//     }
+//     else {
+//         menulist.style.maxHeight = "0px";
+//     }
+// }
+// s
